c0cfe57fddd4e4bda3da79bdc7b1c0077adbe3b7
ProjetBDEEXIAO <julien.reginster@laposte.net>
2017-04-20 16:13:09 +0200

    Delete Footer.php

--- Footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-	<link rel="stylesheet" type="text/css" href="footer.css">
-	<br>
-	<a href="Contact.php" id="contact">Nous contacter</a>
-	<br>
-	<a href="Mentions.php" id="mentions">Mentions l&#233;gales</a>
-</footer>
