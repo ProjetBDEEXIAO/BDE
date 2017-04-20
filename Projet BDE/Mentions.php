ccd4a3199277c7190d080aeb81fdb5d3fac56ef9
ProjetBDEEXIAO <julien.reginster@laposte.net>
2017-04-20 16:11:48 +0200

    Add files via upload

---
+++ Projet BDE/Mentions.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="mentions.css"/>
+        <title>Mentions l&#233;gales</title>
+    </head>
+
+    <body>
+	<?php include("Header.php")?>
+	
+	<h1>MENTIONS LEGALES :</h1>
+
+	<p style="text-align:justify">Conform&#233;ment aux dispositions des articles 6-III et 19 de la Loi n&#176; 2004-575 du 21 juin 2004 pour la Confiance dans l'&#233;conomie num&#233;rique, dite L.C.E.N., nous portons &#224; la connaissance des utilisateurs et visiteurs du site : <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> les informations suivantes :</p>
+
+	<p style="text-align:justify"><strong>1. Informations l&#233;gales :</strong></p>
+
+	<p style="text-align:justify">Statut du propri&#233;taire : <strong>particulier</strong><br />
+	Le Propri&#233;taire est : <strong>Julien Reginster</strong><br />
+	Adresse postale du propri&#233;taire : <strong>55 rue de la tabati&#232re 45100 Orl&#233;ans</strong><br/>
+	<br />
+	Le Cr&#233;ateur du site est : <strong>Julien Reginster</strong><br />
+	Le Responsable de la publication est : <strong>Julien Reginster</strong><br />
+	Contacter le responsable de la publication : <strong>Julien.Reginster@gmail.com</strong><br />
+	Le responsable de la publication est une <strong>personne physique</strong><br />
+	<br />
+	Le Webmaster est : <strong>Julien Reginster</strong><br />
+	Contacter le Webmaster : <strong><a href="mailto:Julien.Reginster@viacesi.fr?subject=Contact � partir des mentions l�gales via le site www.bdecesi.com">Julien.Reginster@viacesi.fr</a></strong><br />
+	L&#146;hebergeur du site est : <strong>Julien Reginster 45100 Orl&#233;ans</strong><br />
+	<strong><u>CREDIT :</u> </strong> Les mentions l&#233;gales ont &#233;t&#233;s g&#233;n&#233;r&#233;es par<strong> <a href="http://www.generer-mentions-legales.com" target="_blank">http://www.generer-mentions-legales.com</a></strong></p>
+
+	<p style="text-align:justify"></p>
+
+	<p style="text-align:justify"><strong>2. Pr&#233;sentation et principe :</strong></p>
+
+	<p style="text-align:justify">Est d&#233;sign&#233; ci-apr&#232s : <strong>Utilisateur</strong>, tout internaute se connectant et utilisant le site susnomm&#233; : <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a>.<br />
+	Le site <a href="http://www.bdecesi.com" target="_blank"><strong>www.bdecesi.com</strong></a><strong> </strong>regroupe un ensemble de services, dans l'&#233;tat,? mis &#224; la disposition des utilisateurs. Il est ici pr&#233;cis&#233; que ces derniers doivent rester courtois et faire preuve de bonne foi tant envers les autres utilisateurs qu'envers le webmaster du site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a>. Le site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> est mis &#224; jour r&#233;guli&#232rement par Julien Reginster.<br />
+	Julien Reginster s&#146;efforce de fournir sur le site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> des informations les plus pr&#233;cises possibles (sous r&#233;serve de modifications apport&#233;es depuis leur mise en ligne), mais ne saurait garantir l'exactitude, la compl&#233;tude et l'actualit&#233; des informations diffus&#233;es sur son site, qu&#146;elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. En cons&#233;quence, l'utilisateur reconna&#238;t utiliser ces informations donn&#233;es (&#224; titre indicatif, non exhaustives et susceptibles d'&#233;voluer) sous sa responsabilit&#233; exclusive.</p>
+
+	<p style="text-align:justify"></p>
+
+	<p style="text-align:justify"><strong>3. Accessibilit&#233; :</strong><br />
+	<br />
+	Le site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> est par principe accessible aux utilisateurs 24/24h, 7/7j, sauf interruption, programm&#233;e ou non, pour les besoins de sa maintenance ou en cas de force majeure. En cas d&#146;impossibilit&#233; d&#146;acc&#232s au service, <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> s&#146;engage &#224; faire son maximum afin de r&#233;tablir l&#146;acc&#232s au service et s&#146;efforcera alors de communiquer pr&#233;alablement aux utilisateurs les dates et heures de l&#146;intervention.? N&#146;&#233;tant soumis qu&#146;&#224; une obligation de moyen, <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> ne saurait &#234;tre tenu pour responsable de tout dommage, quelle qu&#146;en soit la nature, r&#233;sultant d&#146;une indisponibilit&#233; du service.</p>
+
+	<p style="text-align:justify"></p>
+
+	<p style="text-align:justify"><strong style="line-height:1.6em">4. Propri&#233;t&#233; intellectuelle :</strong></p>
+
+	<p style="text-align:justify"><br />
+	Julien Reginster est propri&#233;taire exclusif de tous les droits de propri&#233;t&#233; intellectuelle ou d&#233;tient les droits d&#146;usage sur tous les &#233;l&#233;ments accessibles sur le site, tant sur la structure que sur les textes, images, graphismes, logo, ic?es, sons, logiciels?<br />
+	Toute reproduction totale ou partielle du site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a>, repr&#233;sentation, modification, publication, adaptation totale ou partielle de l'un quelconque de ces &#233;l&#233;ments, quel que soit le moyen ou le proc&#233;d&#233; utilis&#233;, est interdite, sauf autorisation &#233;crite pr&#233;alable de Julien Reginster, propri&#233;taire du site &#224; l'email : <a href="mailto:Julien.Reginster@viacesi.fr?subject=Contact � partir des mentions l�gales via le site www.bdecesi.com">Julien.Reginster@viacesi.fr</a>, &#224; d&#233;faut elle sera consid&#233;r&#233;e comme constitutive d&#146;une contrefa&#231;on et passible de poursuite conform&#233;ment aux dispositions des articles L.335-2 et suivants du Code de Propri&#233;t&#233; Intellectuelle.</p>
+
+	<p style="text-align:justify"><br />
+	<strong>5. Liens hypertextes et cookies :</strong><br />
+	<br />
+	Le site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> contient un certain nombre de liens hypertextes vers d&#146;autres sites (partenaires, informations ?) mis en place avec l&#146;autorisation de Julien Reginster. Cependant, Julien Reginster n&#146;a pas la possibilit&#233; de v&#233;rifier l'ensemble du contenu des sites ainsi visit&#233;s?et d&#233;cline donc toute responsabilit&#233; de ce fait quand aux risques &#233;ventuels de contenus illicites.<br />
+	L&#146;utilisateur est inform&#233; que lors de ses visites sur le site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a>, un ou des cookies sont susceptibles de s&#146;installer automatiquement sur son ordinateur par l'interm&#233;diaire de son logiciel de navigation. Un cookie est un bloc de donn&#233;es qui ne permet pas d'identifier l'utilisateur, mais qui enregistre des informations relatives &#224; la navigation de celui-ci sur le site.?<br />
+	Le param&#233;trage du logiciel de navigation permet d&#146;informer de la pr&#233;sence de cookie et &#233;ventuellement, de la refuser de la mani&#232re d&#233;crite &#224; l&#146;adresse suivante : <a href="http://www.cnil.fr">www.cnil.fr</a>. L&#146;utilisateur peut toutefois configurer le navigateur de son ordinateur pour refuser l&#146;installation des cookies, sachant que le refus d'installation d'un cookie peut entra&#238;ner l&#146;impossibilit&#233; d&#146;acc&#233;der &#224; certains services. Pour tout bloquage des cookies, tapez dans votre moteur de recherche : bloquage des cookies sous IE ou firefox et suivez les instructions en fonction de votre version.</p>
+
+	<p style="text-align:justify"><br />
+	<strong>6. Protection des biens et des personnes - gestion des donn&#233;es personnelles :</strong><br />
+	<br />
+	En France, les donn&#233;es personnelles sont notamment prot&#233;g&#233;es par la loi n&#176; 78-87 du 6 janvier 1978, la loi n&#176; 2004-801 du 6 ao?t 2004, l'article L. 226-13 du Code p&#233;nal et la Directive Europ&#233;enne du 24 octobre 1995.</p>
+
+	<p style="text-align:justify">Sur le site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a>, Julien Reginster ne collecte des informations personnelles ( suivant l'article 4 loi n&#176;78-17 du 06 janvier 1978) relatives &#224; l'utilisateur que pour le besoin de certains services propos&#233;s par le site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a>. L'utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu'il proc&#232de par lui-m&#234;me &#224; leur saisie. Il est alors pr&#233;cis&#233; &#224; l'utilisateur du site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> l&#146;obligation ou non de fournir ces informations.<br />
+	Conform&#233;ment aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative &#224; l&#146;informatique, aux fichiers et aux libert&#233;s, tout utilisateur dispose d&#146;un droit d&#146;acc&#232s, de rectification, de suppression et d&#146;opposition aux donn&#233;es personnelles le concernant. Pour l&#146;exercer, adressez votre demande &#224; <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> par email : <strong><a href="mailto:moi@viacesi.fr?subject=Contact � partir des mentions l�gales via le site www.bdecesi.com">Julien.Reginster@gmail.com</a></strong> ou?par &#233;crit?d?ment sign&#233;e, accompagn&#233;e d&#146;une copie du titre d&#146;identit&#233; avec signature du titulaire de la pi&#232ce, en pr&#233;cisant l&#146;adresse &#224; laquelle la r&#233;ponse doit &#234;tre envoy&#233;e.</p>
+
+	<p style="text-align:justify">Aucune information personnelle de l'utilisateur du site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> n'est publi&#233;e &#224; l'insu de l'utilisateur, &#233;chang&#233;e, transf&#233;r&#233;e, c&#233;d&#233;e ou vendue sur un support quelconque &#224; des tiers. Seule l'hypoth&#232se du rachat du site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> et de ses droits autorise Julien Reginster &#224; transmettre les dites informations &#224; l'&#233;ventuel acqu&#233;reur qui serait &#224; son tour tenu?&#224; la m&#234;me obligation de conservation et de modification des donn&#233;es vis &#224; vis de l'utilisateur du site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a>.<br />
+	Le site <a href="http://www.bdecesi.com" target="_blank">www.bdecesi.com</a> est d&#233;clar&#233; &#224; la CNIL sous le num&#233;ro 5513548565.</p>
+
+	<p style="text-align:justify">Les bases de donn&#233;es sont prot&#233;g&#233;es par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative &#224; la protection juridique des bases de donn&#233;es.</p>
+
+	
+	<p style="text-align:center"><span style="color:#f00">Si vous utilisez nos Mentions L&#233;gales, vous vous engagez &#224; laisser le lien <strong>Cr&#233;dit :</strong> <a href="http://www.generer-mentions-legales.com">www.generer-mentions-legales.com</a>?<br />
+	Par avance merci pour votre compr&#233;hension et le respect du travail effectu&#233; par toute l'&#233;quipe de : <a href="http://www.generer-mentions-legales.com">www.generer-mentions-legales.com</a></span></p>
+
+	<?php include("Footer.php")?>
+    </body>
+</html>
