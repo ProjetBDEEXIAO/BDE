1a452d649a53cb546a48304536a2aa47c4e97a7f
ProjetBDEEXIAO <julien.reginster@laposte.net>
2017-04-20 16:08:05 +0200

    Projet

---
+++ Contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="contact.css"/>
+        <title>Nous contacter</title>
+    </head>
+
+    <body>
+	<?php include("Header.php")?>
+	
+	<ul>Adresse mail:
+			<li>julien@viacesi.fr</li>
+			<li>corentin@viacesi.fr</li>
+			<li>josselin@viacesi.fr</li>
+			<li>zakari@viacesi.fr</li>
+	</ul>
+	
+	<ul>Téléphone:
+			<li>06 35 05 08 ..</li>
+			<li>06 45 98 68 ..</li>
+			<li>06 58 97 48 ..</li>
+			<li>06 78 54 88 ..</li>
+	</ul>
+	
+	<?php include("Footer.php")?>
+    </body>
+</html>
---
+++ Footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<link rel="stylesheet" type="text/css" href="footer.css">
+	<br>
+	<a href="Contact.php" id="contact">Nous contacter</a>
+	<br>
+	<a href="Mentions.php" id="mentions">Mentions l&#233;gales</a>
+</footer>
