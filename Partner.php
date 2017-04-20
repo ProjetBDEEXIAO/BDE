8417543a89123a1bd74fa373f888f479dbb6f6e7
ProjetBDEEXIAO <julien.reginster@laposte.net>
2017-04-20 14:58:47 +0200

    Add files via upload

---
+++ Partner.php
@@ -0,0 +1,21 @@
+<?php include("Header.php");?>
+
+<!DOCTYPE html>
+<html>
+<header>
+<title>Partenariat</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="Partner.css">
+<link rel="stylesheet" type="text/css" href="header.css">
+</header>
+
+
+<section>  
+<p class="titre">Le partenariat</p>
+   <p>Le BDE a la chance de possèder quelques partenaires de confiance, dans le cadre des activités que nous vous proposons, nous il est possible d'avoir certaine réduction ou d'avoir une happy hour plus longue sous présentation de votre carte étudiante du CESI.</p><p>Vous pouvez retrouver l'ensemble de nos partenaires ici :<br>.<br>.<br>.<br>.<br>.<br>.<br> 
+   </p>
+    </section>
+<!-- Footer -->
+<?php include("Footer.php")?>
+</body>
+</html>
