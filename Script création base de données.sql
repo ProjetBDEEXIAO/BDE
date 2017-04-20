7999aa607643628b8ba829ac2e14959ff04d576b
ProjetBDEEXIAO <julien.reginster@laposte.net>
2017-04-20 17:03:48 +0200

    Add files via upload

---
+++ Script création base de données.sql
@@ -0,0 +1,143 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+CREATE DATABASE website_project;
+USE website_project ; 
+#------------------------------------------------------------
+# Table: Users
+#------------------------------------------------------------
+
+CREATE TABLE Users(
+        id_users       int (11) Auto_increment  NOT NULL ,
+        first_name     Varchar (50) NOT NULL ,
+        name           Varchar (50) ,
+        formation      Varchar (25) ,
+        email          Varchar (50) NOT NULL ,
+        password       Varchar (50) NOT NULL ,
+        premium_status Bool NOT NULL ,
+        PRIMARY KEY (id_users )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Propos_Activity
+#------------------------------------------------------------
+
+CREATE TABLE Propos_Activity(
+        id_propos         int (11) Auto_increment  NOT NULL ,
+        activity_proposed Varchar (50) NOT NULL ,
+        description_text  Varchar (200) NOT NULL ,
+        id_users          Int NOT NULL ,
+        PRIMARY KEY (id_propos )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Event_BDE
+#------------------------------------------------------------
+
+CREATE TABLE Event_BDE(
+        id_event      int (11) Auto_increment  NOT NULL ,
+        activity_name Varchar (50) NOT NULL ,
+        detail_text   Varchar (200) NOT NULL ,
+        event_date1   Date NOT NULL ,
+        event_date2   Date NOT NULL ,
+        hour1         Datetime NOT NULL ,
+        hour2         Datetime NOT NULL ,
+        vote1         Int NOT NULL ,
+        vote2         Int NOT NULL ,
+        PRIMARY KEY (id_event )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Picture
+#------------------------------------------------------------
+
+CREATE TABLE Picture(
+        id_picture     int (11) Auto_increment  NOT NULL ,
+        picture        Varchar (100) NOT NULL ,
+        uploading_date Date NOT NULL ,
+        id_event       Int NOT NULL ,
+        PRIMARY KEY (id_picture )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Store
+#------------------------------------------------------------
+
+CREATE TABLE Store(
+        id_store        int (11) Auto_increment  NOT NULL ,
+        product_name    Varchar (50) NOT NULL ,
+        price           Int NOT NULL ,
+        notice          Varchar (200) NOT NULL ,
+        product_picture Varchar (100) NOT NULL ,
+        quantity        Int NOT NULL ,
+        clothes         Bool NOT NULL ,
+        size            Varchar (25) NOT NULL ,
+        PRIMARY KEY (id_store )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Orders
+#------------------------------------------------------------
+
+CREATE TABLE Orders(
+        id_orders       int (11) Auto_increment  NOT NULL ,
+        total_price     Int NOT NULL ,
+        payment_status  Varchar (50) NOT NULL ,
+        delivery_status Varchar (50) NOT NULL ,
+        date_orders     Date NOT NULL ,
+        id_users        Int NOT NULL ,
+        PRIMARY KEY (id_orders )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Commentaries
+#------------------------------------------------------------
+
+CREATE TABLE Commentaries(
+        id_commentary int (11) Auto_increment  NOT NULL ,
+        comment_text  Varchar (200) NOT NULL ,
+        comment_date  Date NOT NULL ,
+        id_users      Int NOT NULL ,
+        id_picture    Int NOT NULL ,
+        PRIMARY KEY (id_commentary )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Like_Button
+#------------------------------------------------------------
+
+CREATE TABLE Like_Button(
+        like_but   Bool NOT NULL ,
+        id_picture Int NOT NULL ,
+        id_users   Int NOT NULL ,
+        PRIMARY KEY (id_picture ,id_users )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Orders_Content
+#------------------------------------------------------------
+
+CREATE TABLE Orders_Content(
+        amount    Int NOT NULL ,
+        id_orders Int NOT NULL ,
+        id_store  Int NOT NULL ,
+        PRIMARY KEY (id_orders ,id_store )
+)ENGINE=InnoDB;
+
+ALTER TABLE Propos_Activity ADD CONSTRAINT FK_Propos_Activity_id_users FOREIGN KEY (id_users) REFERENCES Users(id_users);
+ALTER TABLE Picture ADD CONSTRAINT FK_Picture_id_event FOREIGN KEY (id_event) REFERENCES Event_BDE(id_event);
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_id_users FOREIGN KEY (id_users) REFERENCES Users(id_users);
+ALTER TABLE Commentaries ADD CONSTRAINT FK_Commentaries_id_users FOREIGN KEY (id_users) REFERENCES Users(id_users);
+ALTER TABLE Commentaries ADD CONSTRAINT FK_Commentaries_id_picture FOREIGN KEY (id_picture) REFERENCES Picture(id_picture);
+ALTER TABLE Like_Button ADD CONSTRAINT FK_Like_Button_id_picture FOREIGN KEY (id_picture) REFERENCES Picture(id_picture);
+ALTER TABLE Like_Button ADD CONSTRAINT FK_Like_Button_id_users FOREIGN KEY (id_users) REFERENCES Users(id_users);
+ALTER TABLE Orders_Content ADD CONSTRAINT FK_Orders_Content_id_orders FOREIGN KEY (id_orders) REFERENCES Orders(id_orders);
+ALTER TABLE Orders_Content ADD CONSTRAINT FK_Orders_Content_id_store FOREIGN KEY (id_store) REFERENCES Store(id_store);
---
+++ Script remplissage.sql
@@ -0,0 +1,54 @@
+INSERT INTO Users (id_users, first_name, name, formation, premium_status, email , password)
+ VALUES
+	('','Julien', 'REGINSTER','A2', '1', 'julien.reginster@viacesi.fr', '1234julien'),
+	('','Josselin', 'KAPFER','A2', '1', 'josselin.kapfer@viacesi.fr', '1234corentin'),
+	('','Corentin', 'BENOIST','A2', '1', 'corentin.benoist@viacesi.fr', '1234josselin'),
+	('','Zakari', 'HAMSIOU', 'A2', '1', 'zakari.hamsiou@viacesi.fr', '1234zakari'),
+	('','Jean', 'VALJEAN','QSE', '1', 'jean.valjean@viacesi.fr', '1234jean');
+
+
+INSERT INTO Propos_Activity (id_propos, activity_proposed, description_text, id_users)
+ VALUES
+	('','PAINTBALL', 'because it’s awesome!!!!','2'),
+	('','Rallye des bars', 'Its Drinking time !!!','1'),
+	('','Cinéma', 'Fast & Furious 8 or Power Rangers','4'),
+	('','Drinking like a mad men', 'At Hendrix PUB :) :) :)','1'),
+	('','Au Pavillon', 'I feel it coming <3<3<3<3 ','3');
+
+INSERT INTO Event_BDE (id_event, activity_name, detail_text, event_date1, event_date2, vote1, vote2, hour1, hour2)
+ VALUES
+	('', 'CINÉMA - FAST & FURIOUS 8', 'the movie where Vin Diesel a.k.a Dominic Toretto finally died damn... Spoilers Alert', '2017-04-21', '2017-05-04', '14:00:00', '16:00:00', '0', '0'),
+	('', 'RALLYE DES BARS - Bourgogne', 'Au Petit Barça/ Hendrix PUB -> Rue de Bourgogne because Julien worths it', '2017-04-21', '2017-04-28', '19:00:00', '21:00:00', '0', '0') ;
+
+INSERT INTO Picture (id_picture, picture, uploading_date, id_event)
+ VALUES
+	('', 'file:///C:\Users\Zakari-Hamsiou\Desktop\avatar\Fast 8.jpg', '04-05-2017', '1');
+
+INSERT INTO Commentaries (id_commentary, comment_text, comment_date, id_users, id_picture)
+ VALUES
+	('', 'OMG did you go watch this crap… ?!', '2017-05-04', '1','1'),
+	('', 'Seriously, julien, can you shut up ?', '2017-05-04', '2','1'),
+	('', 'I agree with him', '2017-05-04', '3','1'),
+	('', 'Great movie !', '2017-05-04', '4','1');
+
+INSERT INTO Like_Button (like_but, id_picture, id_users)
+	VALUES
+	('0','1','1'),
+	('1','1','2'),
+	('1','1','3'),
+	('1','1','4'),
+	('0','1','5');	
+
+INSERT INTO Store (id_store, product_name, price, notice, product_picture, quantity, clothes, size)
+	VALUES
+	('','Thermos EINSENBERG','25', 'Thermostat to warm cold & bad mornings !', 'C:\Users\Zakari-Hamsiou\Desktop\avatar\Thermos.jpg', '150', '0', ''), 
+	('','T-Shirt 404 error Beer','15', 'a official T-Shirt for programming code and beer addict !', 'C:\Users\Zakari-Hamsiou\Desktop\avatar\404 shirt.jpg', '75', '1', 'ALL SIZE');
+
+INSERT INTO Orders (id_orders, total_price, payment_status, delivery_status, date_orders, id_users)
+	VALUES
+	('','115','Payment in process ...', 'Delivered 12-05-2017', '2017-04-24', '3');
+
+INSERT INTO Orders_Content (amount, id_orders, id_store)
+	VALUES
+	('4','1','1'),
+	('1','1','2');
