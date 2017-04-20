444bbb90639e51fee1b3b946c6ef4e614de876fa
ProjetBDEEXIAO <julien.reginster@laposte.net>
2017-04-20 15:25:34 +0200

    apb(1)

---
+++ BDE-apb/Header.php
@@ -0,0 +1,20 @@
+<meta charset= "UTF-8">
+<header>
+	<h1>BDE<br>CESI<br>Orléans <a href="Accueil.php"><img src="LogoBDE.png" id="Logo"/></a></h1>
+	<link rel="stylesheet" type="text/css" href="header.css"> <!-- Create a link with a ccs sheet. -->
+</header>
+
+<nav>
+	<ul class="menu horizontal-list"> <!-- Class_menu_horiz create a transversale listed menu. -->
+		<li><a href="Activites.php">Activités</a> <!-- List of links to 3 pages of activities. -->
+			<ul>
+				<li><a href="Activites.php#ActProp">Activités proposées</a></li> <!-- link with a page where BDE propose activities. -->
+				<li><a href="Activites.php#PropAct">Proposer une activité</a></li> <!-- link with a page where users can propose activities. -->
+				<li><a href="Activites.php#Histo">Historique des activités</a></li> <!-- link with a page where it show an historic of activities. -->
+			</ul>
+		</li>
+		<a href="shop.php">Boutique</a> <!-- link with a page to access to web store. -->
+		<a href="apb.php">Á propos du BDE</a> <!-- link with a page where users can have access for BDE informations. -->
+		<a href="Partner.php">Partenariat</a> <!-- link with a page where it show an historic of activities. -->
+	</ul>
+</nav>
---
+++ BDE-apb/apb.php
@@ -0,0 +1,141 @@
+<?php include("Header.php");?>
+<?php include("Footer.php");?>
+
+<!DOCTYPE html>                                                      <!-- Describes  the type of document.  -->
+<html>                                                               <!-- html tag is where the html code starts.  -->
+<header>                                                             <!-- header tag where the links with headers are made. -->
+<title>Á Propos De Nous</title>                                      <!-- Describes  the type of document.  -->
+<meta charset= "UTF-8">                                              <!-- Describes  the type of encryption.  -->
+<link rel="stylesheet" href="APB.css">                               <!-- Create a link with a ccs sheet. -->
+<link rel="stylesheet" type="text/css" href="header.css">          <!-- Create a link with a header. -->
+</header>
+
+<section>                                         <!-- section tag is where the text of one section starts.  -->
+<p class="titre">Á Propos De Nous</p>             <!-- class_titre helps to set up the title with the help of ccs sheet.  -->
+<p class="sous_titre">A) Présentation</p>         <!-- class_sous_titre helps to set up the subtitles with the help of ccs sheet.  -->
+   <p>    Le B.D.E est l'acronyme de bureau des élèves<br><br>
+          Mais qu'est-ce-que le bureau des élèves ?<br><br>
+          Le bureau des élèves est une association à but non lucratif qui a pour mission principale : <br>
+          l'organisation d'événements et/ou d'activités rentrants dans le cadre de la vie associative du CESI.<br>
+          Le CESI est un centre de formations qui propose plusieurs formations et diplômes pour des futurs cadres en entreprise.<br><br>
+          La pluridisplinarité au sein du CESI est l'essor de la diversité et de cohésion que l'on retrouve dans une entreprise. <br>
+          Elle permet aux étudiants, la création d'un réseau à l'intérieur de l'école, d'où l'importance d'un BDE CESI.<br>
+          Avant 2016, le BDE était une exclusivité des internes du CESI. Il était majoritairement représenté par les Exars.<br>
+          Á la rentrée 2016, L'ancien BDE est dissout et laisse place à un nouveau BDE CESI.<br>
+          Ce changement donne un nouveau souffle au BDE. C'est ainsi que cette cohésion recherchée peut aboutir.<br><br>
+          
+          Nous, Membre du BDE, jurons solennellement de servir au mieux les intérêts des étudiants du CESI.<br>
+          Et jurons de vous, étudiants, proposer des évenements qui vous permettront d'accroitre votre réseau,<br> 
+          tout en assurant votre divertissement.<br>
+          </p>
+<p class="sous_titre">B) Notre Équipe</p>
+<article>                                                           <!-- article tag is used to create others specials sections.  -->
+	<h4>La Présidente du BDE: Élodie LE GUILLOUX</h4>
+	<h5>Formation : CESI Alternance Promotion : RRH24</h5>
+  <h5>@ email  : elodie.leguilloux@viacesi.fr</h5>
+</article>
+
+<article> 
+  <h4>Le Président suppléant: Kerim YASAR</h4>
+  <h5>Formation : CESI EXIA Promotion : A1</h5>
+  <h5>@ email  : kerim.yasar@viacesi.fr</h5>
+</article> 
+
+<article> 
+  <h4>Le Vice-Président: Mathias ARREDONDO</h4>
+  <h5>Formation : CESI EXIA Promotion : A2</h5>
+  <h5>@ email  : mathias.arredondo@viacesi.fr</h5>
+</article> 
+
+<article> 
+  <h4>Le Vice-Président suppléant: Corentin BENOIST</h4>
+  <h5>Formation : CESI EXIA Promotion : A2</h5>
+  <h5>@ email  : corentin.benoist@viacesi.fr</h5>
+</article> 
+
+<article> 
+  <h4>Le Trésorier : Nathanaël ANSTETT</h4>
+  <h5>Formation : CESI EXIA Promotion : A2</h5>
+  <h5>@ email  : nathanel.anstett@viacesi.fr</h5>
+</article> 
+
+<article> 
+  <h4>La Trésorière suppléante: Sarah DRAZEK</h4>
+  <h5>Formation : CESI Alternance Promotion : MSQSE</h5>
+  <h5>@ email  : sarah.drazek@viacesi.fr</h5>
+</article> 
+
+<article> 
+  <h4>Le Secrétaire: Thomas DOMINGUES</h4>
+  <h5>Formation : CESI EXIA Promotion : A2</h5>
+  <h5>@ email  : thomas.domingues@viacesi.fr</h5>
+</article> 
+
+<article> 
+    <h4>La Secrétaire suppléante: Sarah DRAZEK</h4>
+  <h5>Formation : CESI Alternance Promotion : MSQSE</h5>
+  <h5>@ email  : sarah.drazek@viacesi.fr</h5>
+</article> 
+
+<article> 
+    <h4>Le Responsable des évènements: Benoît BASCHOU</h4>
+  <h5>Formation : CESI EXIA Promotion : A2</h5>
+  <h5>@ email  : benoit.baschou@viacesi.fr</h5>
+</article> 
+
+<article> 
+    <h4>Le Responsable adjoint des évènements: Khalil HABBACHI</h4>
+  <h5>Formation : CESI EXIA Promotion : MSQSE</h5>
+  <h5>@ email  : khalil.habbachi@viacesi.fr</h5>
+</article> 
+
+<article> 
+    <h4>La Responsable adjointe des évènements: Élodie LE GUILLOUX</h4>
+  <h5>Formation : CESI Alternance Promotion : RRH24</h5>
+  <h5>@ email  : elodie.leguilloux@viacesi.fr</h5>
+</article> 
+
+<article> 
+    <h4>Le Responsable des activités: Guillaume VIGNIAT</h4>
+  <h5>Formation : CESI EXIA Promotion : MSQSE</h5>
+  <h5>@ email  : guillaume.vigniat@viacesi.fr</h5>
+</article> 
+
+<article> 
+    <h4>Le Responsable adjoint des activités: Benoît BASCHOU</h4>
+  <h5>Formation : CESI EXIA Promotion : A2</h5>
+  <h5>@ email  : benoit.baschou@viacesi.fr</h5>
+</article> 
+
+<article>
+    <h4>Le Responsable adjoint des activité: Philippe BILLARD</li></ul></h4>
+  <h5>Formation : CESI EXIA Promotion : A3</h5>
+  <h5>@ email  : philippe.billard@viacesi.fr</h5>
+</article>
+
+<br><br>
+<p class="sous_titre">Plus d'informations</p>                       <!-- class_conteneur helps to set up links to CESI websites  -->
+<div id="conteneur">
+    <div class="element1"><a href="https://www.eicesi.fr/">EI.CESI</a></div>
+    <div class="element2"><a href="https://exia.cesi.fr/">EXIA.CESI</a></div>
+    <div class="element3"><a href="https://www.cesi-alternance.fr/">CESI.ALTERNANCE</a></div>
+    <div class="element4"><a href="https://www.cesi-entreprises.fr/">CESI.ENTREPRISES</a></div>
+    <div class="element5"><a href="https://recherche.cesi.fr/">CESI.RECHERCHE</a></div>
+    <div class="element6"><a href="https://www.cesi-certification.fr/">CESI.CERTIFICATION</a></div>
+</div>
+
+
+    </section>
+<!-- Footer -->
+<footer>        <!-- footer tag is used to set up the legal notices.  -->
+  
+</footer>
+
+
+<!--
+To use this code on your website, get a free API key from Google.
+Read more at: https://www.schools.com/graphics/google_maps_basic.asp
+-->
+
+</body>
+</html>
